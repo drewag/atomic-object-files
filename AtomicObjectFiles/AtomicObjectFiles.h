92eb3efd492a0a39dfd5f12d6dc76f1a851ab062
Andrew Wagner <andrew@drewagllc.com>
2015-10-09 10:08:19 -0600

    Initial Commit

---
+++ AtomicObjectFiles/AtomicObjectFiles.h
@@ -0,0 +1,19 @@
+//
+//  AtomicObjectFiles.h
+//  AtomicObjectFiles
+//
+//  Created by Andrew J Wagner on 10/9/15.
+//  Copyright © 2015 Drewag, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AtomicObjectFiles.
+FOUNDATION_EXPORT double AtomicObjectFilesVersionNumber;
+
+//! Project version string for AtomicObjectFiles.
+FOUNDATION_EXPORT const unsigned char AtomicObjectFilesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AtomicObjectFiles/PublicHeader.h>
+
+
